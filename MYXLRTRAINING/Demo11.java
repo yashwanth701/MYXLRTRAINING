16be225889196ddf4029def29a00600a1f046806
yashwanth701 <meelayashwanth2004@gmail.com>
2025-01-09 21:15:35 +0530

    Add files via upload

---
+++ MYXLRTRAINING/Demo11.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Demo11 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        float r=obj.nextFloat();
+        double area = Math.PI * r * r;
+        System.out.println("Area of circle: "+area);
+
+
+    }
+
+    
+}
